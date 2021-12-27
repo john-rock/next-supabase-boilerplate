ec4913d62d65e633b86d584b2618c39412ed71c7
John Rock <johnmrock.jr@gmail.com>
2021-12-27 18:08:55 -0500

    Fix previous commit message

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,48 @@
-import { useState, useEffect } from 'react'
-import Link from 'next/link'
-import { supabase } from '../api'
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { supabase } from '../api';
 
 export default function Home() {
-  const [posts, setPosts] = useState([])
-  const [loading, setLoading] = useState(true)
+  // Set the initial state
+  const [posts, setPosts] = useState([]);
+
+  // Set loading state
+  const [loading, setLoading] = useState(true);
+
+  // Setup realtime updates from the database
   useEffect(() => {
-    fetchPosts()
+    fetchPosts();
     const mySubscription = supabase
       .from('posts')
       .on('*', () => fetchPosts())
-      .subscribe()
-    return () => supabase.removeSubscription(mySubscription)
-  }, [])
+      .subscribe();
+    return () => supabase.removeSubscription(mySubscription);
+  }, []);
+
+  // Fetch posts from the database
   async function fetchPosts() {
-    const { data, error } = await supabase
-      .from('posts')
-      .select()
-    setPosts(data)
-    setLoading(false)
+    const { data, error } = await supabase.from('posts').select();
+    setPosts(data);
+    setLoading(false);
   }
-  if (loading) return <p className="text-2xl">Loading ...</p>
-  if (!posts.length) return <p className="text-2xl">No posts.</p>
+
+  // Handle loading state
+  if (loading) return <p className='text-2xl'>Loading ...</p>;
+
+  // Handle no data
+  if (!posts.length) return <p className='text-2xl'>No posts.</p>;
+
   return (
     <div>
-      <h1 className="text-3xl font-semibold tracking-wide mt-6 mb-2">Posts</h1>
-      {
-        posts.map(post => (
-          <Link key={post.id} href={`/posts/${post.id}`}>
-            <div className="cursor-pointer border-b border-gray-300	mt-8 pb-4">
-              <h2 className="text-xl font-semibold">{post.title}</h2>
-              <p className="text-gray-500 mt-2">Author: {post.user_email}</p>
-            </div>
-          </Link>)
-        )
-      }
+      <h1 className='text-3xl font-semibold tracking-wide mt-6 mb-2'>Posts</h1>
+      {posts.map((post) => (
+        <Link key={post.id} href={`/posts/${post.id}`}>
+          <div className='cursor-pointer border-b border-gray-300	mt-8 pb-4'>
+            <h2 className='text-xl font-semibold'>{post.title}</h2>
+            <p className='text-gray-500 mt-2'>Author: {post.user_email}</p>
+          </div>
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
