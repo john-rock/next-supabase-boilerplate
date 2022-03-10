13994528fab3a117418e0393f3eeefdf55b4f6da
John Rock <johnmrock.jr@gmail.com>
2022-03-10 14:47:50 -0500

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,15 @@
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import { supabase } from '../api';
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { supabase } from "../supabase-client";
+import { useSession } from "../utils/user-context";
 
 export default function Home() {
-  // Set the initial state
-  const [posts, setPosts] = useState([]);
-
-  // Set loading state
-  const [loading, setLoading] = useState(true);
-
-  // Setup realtime updates from the database
-  useEffect(() => {
-    fetchPosts();
-    const mySubscription = supabase
-      .from('posts')
-      .on('*', () => fetchPosts())
-      .subscribe();
-    return () => supabase.removeSubscription(mySubscription);
-  }, []);
-
-  // Fetch posts from the database
-  async function fetchPosts() {
-    const { data, error } = await supabase.from('posts').select();
-    setPosts(data);
-    setLoading(false);
-  }
-
-  // Handle loading state
-  if (loading) return <p className='text-2xl'>Loading ...</p>;
-
-  // Handle no data
-  if (!posts.length) return <p className='text-2xl'>No posts.</p>;
-
+  const { session } = useSession();
   return (
     <div>
-      <h1 className='text-3xl font-semibold tracking-wide mt-6 mb-2'>Posts</h1>
-      {posts.map((post) => (
-        <Link key={post.id} href={`/posts/${post.id}`}>
-          <div className='cursor-pointer border-b border-gray-300	mt-8 pb-4'>
-            <h2 className='text-xl font-semibold'>{post.title}</h2>
-            <p className='text-gray-500 mt-2'>Author: {post.user_email}</p>
-          </div>
-        </Link>
-      ))}
+      <h1 className="text-3xl font-semibold tracking-wide mt-6 mb-2">
+        NextJS Supabase Starter
+      </h1>
     </div>
   );
 }
---
+++ utils/user-context.js
@@ -0,0 +1,44 @@
+import { useEffect, useState, createContext, useContext } from "react";
+import { supabase } from "../supabase-client";
+
+export const UserContext = createContext(null);
+
+export function UserContextProvider(props) {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    // Get user session info
+    const session = supabase.auth.session();
+    setSession(session);
+
+    // Watch for auth changes and hit auth api if changed
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        setSession(session);
+        await fetch("/api/auth", {
+          method: "POST",
+          body: JSON.stringify({ event, session }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      }
+    );
+
+    return () => {
+      authListener?.unsubscribe();
+    };
+  }, []);
+
+  const value = {
+    session,
+  };
+
+  return <UserContext.Provider value={value} {...props} />;
+}
+
+// Hook to get user session data
+export function useSession() {
+  const context = useContext(UserContext);
+  return context;
+}
